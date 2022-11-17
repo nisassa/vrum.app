c2e017b75645dc13579bacca3b98f228334de660
adrian.s <solomon.adrian.92@gmail.com>
2022-11-17 12:46:37 +0200

    Register view, add 3 column view

--- src/App.js
+++ src/App.js
@@ -1,27 +1,23 @@
 import './App.css';
 import Nav from './Nav';
-import Register from "./Pages/Register";
-import { RegisterProvider } from "./Pages/Parts";
-import Home from "./Pages/Home";
+import Register from './Pages/Register';
+import { RegisterProvider } from './Pages/Parts';
+import Home from './Pages/Home';
 
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-} from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
-import React from "react";
+import React from 'react';
 
 function App() {
   return (
-    <div className="App">
+    <div className='App'>
       <Router>
         <Nav />
         <Routes>
-          <Route path="/register" element={ <Register/> } />
-          <Route path="/register/provider" element={ <RegisterProvider/> } />
+          <Route path='/register' element={<Register />} />
+          <Route path='/register/provider' element={<RegisterProvider />} />
           {/*<Route path="/register/client" element={ <RegisterClient/> } />*/}
-          <Route path="/" element={ <Home/> } />
+          <Route path='/' element={<Home />} />
         </Routes>
       </Router>
     </div>
--- src/Pages/Register.js
+++ src/Pages/Register.js
@@ -1,39 +1,43 @@
-import React from "react";
+import React from 'react';
 
-import {
-    Link
-} from "react-router-dom";
+import { Link } from 'react-router-dom';
 
 function Register() {
-    return (
-        <div className="container my-4 mx-auto px-4 md:px-12">
-            <div className="bg-yellow-400 p-4 mb-4">
-                <h1 className="text-white">Register</h1>
+  return (
+    <div className='container my-4 mx-auto px-4 md:px-12'>
+      <div className='bg-yellow-400 p-4 mb-4'>
+        <h1 className='text-white'>Register</h1>
+      </div>
+      <div className='flex flex-col md:flex-row items-center w-full mb-8 space-x-4'>
+        <div className='w-full md:w-1/2 bg-red-100 rounded-lg shadow-md mb-4 md:mb-0'>
+          <div className='flex items-center px-12 py-8'>
+            <div className='flex flex-col items-start ml-4'>
+              <h1>Register as a client</h1>
+              <Link
+                className='py-2 px-4 my-4 bg-red-500 text-white font-semibold rounded-lg shadow-md hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-blue-400 focus:ring-opacity-75'
+                to='/register/provider'
+              >
+                Start using the services
+              </Link>
             </div>
-            <div className="flex flex-col md:flex-row items-center w-full mb-8 space-x-4">
-                <div className="w-full md:w-1/2 bg-red-100 rounded-lg shadow-md mb-4 md:mb-0">
-                    <div className="flex items-center px-12 py-8">
-                        <div className="flex flex-col items-start ml-4">
-                            <h1>Register as a client</h1>
-                            <Link
-                                className="py-2 px-4 my-4 bg-red-500 text-white font-semibold rounded-lg shadow-md hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-blue-400 focus:ring-opacity-75"
-                                to="/register/provider">Start using the services</Link>
-                        </div>
-                    </div>
-                </div>
-                <div className="w-full md:w-1/2 bg-red-100 rounded-lg shadow-md mb-4 md:mb-0">
-                    <div className="flex items-center px-12 py-8">
-                        <div className="flex flex-col items-start ml-4">
-                            <h1>Register as a provider</h1>
-                            <Link
-                                className="py-2 px-4 my-4 bg-blue-500 text-white font-semibold rounded-lg shadow-md hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-400 focus:ring-opacity-75"
-                                to="/register/provider">Register New Provider</Link>
-                        </div>
-                    </div>
-                </div>
-            </div>
+          </div>
         </div>
-    )
+        <div className='w-full md:w-1/2 bg-red-100 rounded-lg shadow-md mb-4 md:mb-0'>
+          <div className='flex items-center px-12 py-8'>
+            <div className='flex flex-col items-start ml-4'>
+              <h1>Register as a provider</h1>
+              <Link
+                className='py-2 px-4 my-4 bg-blue-500 text-white font-semibold rounded-lg shadow-md hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-400 focus:ring-opacity-75'
+                to='/register/provider'
+              >
+                Register New Provider
+              </Link>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Register;

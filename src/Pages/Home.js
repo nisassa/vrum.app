fa60905ea45b84573ac25c0b1397d826b415822c
adrian.s <solomon.adrian.92@gmail.com>
2022-11-17 16:23:08 +0200

    fix home layout

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 
 function Home() {
   return (
-    <div className='container my-4 mt-1o px-4 md:px-12'>
+    <div className='container mx-auto my-4 mt-1o px-4 md:px-12'>
       <div className='bg-yellow-400 p-4 mb-4'>
         <h1 className='text-white'>Home</h1>
       </div>

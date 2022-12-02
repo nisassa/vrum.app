e97a4a715d471ab2e5fa0acc9abc2f209ddc611c
Adrian.S <solomon.adrian.92@gmail.com>
2022-12-02 12:35:07 +0200

    Change login design

--- src/Pages/Auth/Login.js
+++ src/Pages/Auth/Login.js
@@ -62,14 +62,9 @@ function Login() {
       <main className='bg-white'>
         <div className='relative md:flex'>
           {/* Content */}
-          <div className='md:w-1/2'>
-            <div className='min-h-screen h-full flex flex-col after:flex-1'>
+          <div className='w-full'>
+            <div className='min-h-screen h-full flex items-center'>
               {/* Header */}
-              <div className='flex-1'>
-                <div className='flex items-center justify-between h-16 px-4 sm:px-6 lg:px-8'>
-                  {/* Logo */}
-                </div>
-              </div>
 
               <div className='max-w-sm mx-auto px-4 py-8'>
                 <h1 className='text-3xl text-slate-800 font-bold mb-6'>
@@ -200,25 +195,6 @@ function Login() {
           </div>
 
           {/* Image */}
-          <div
-            className='hidden md:block absolute top-0 bottom-0 right-0 md:w-1/2'
-            aria-hidden='true'
-          >
-            <img
-              className='object-cover object-center w-full h-full'
-              src={AuthImage}
-              width='760'
-              height='1024'
-              alt='Authentication'
-            />
-            <img
-              className='absolute top-1/4 left-0 -translate-x-1/2 ml-8 hidden lg:block'
-              src={AuthDecoration}
-              width='218'
-              height='224'
-              alt='Authentication decoration'
-            />
-          </div>
         </div>
       </main>
     </>

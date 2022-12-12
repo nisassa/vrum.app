da1db9cbb5b613c9e08aa2f6ee11dc1c36daad33
Adrian.S <solomon.adrian.92@gmail.com>
2022-12-12 15:28:02 +0200

    Remove expand sidebar button

--- src/Pages/Provider/partials/Sidebar.jsx
+++ src/Pages/Provider/partials/Sidebar.jsx
@@ -540,25 +540,6 @@ function Sidebar({ sidebarOpen, setSidebarOpen }) {
             </ul>
           </div>
         </div>
-
-        {/* Expand / collapse button */}
-        <div className='pt-3 hidden lg:inline-flex 2xl:hidden justify-end mt-auto'>
-          <div className='px-3 py-2'>
-            <button onClick={() => setSidebarExpanded(!sidebarExpanded)}>
-              <span className='sr-only'>Expand / collapse sidebar</span>
-              <svg
-                className='w-6 h-6 fill-current rotate-180'
-                viewBox='0 0 24 24'
-              >
-                <path
-                  className='text-slate-400'
-                  d='M19.586 11l-5-5L16 4.586 23.414 12 16 19.414 14.586 18l5-5H7v-2z'
-                />
-                <path className='text-slate-600' d='M3 23H1V1h2z' />
-              </svg>
-            </button>
-          </div>
-        </div>
       </div>
     </div>
   );

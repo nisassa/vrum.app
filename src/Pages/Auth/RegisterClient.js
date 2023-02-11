65af76b743713a344e344de318690b278c5f8cc2
Adrian.S <solomon.adrian.92@gmail.com>
2023-02-11 10:38:34 +0200

    Redesign client register form

--- src/Pages/Auth/RegisterClient.js
+++ src/Pages/Auth/RegisterClient.js
@@ -51,8 +51,8 @@ function RegisterClient() {
             Register Client
           </h1>
         </div>
-        <div className='bg-white shadow-lg rounded-sm mb-8 flex flex-col md:flex-row items-center w-full mb-8 space-x-4'>
-          <div className='w-full md:w-1/1  rounded-lg shadow-md mb-4 md:mb-0 px-4 py-4'>
+        <div className='bg-white bg-white overflow-auto max-w-xl w-full max-h-full rounded shadow-lg mx-auto '>
+          <div className='rounded-lg shadow-md mb-4 md:mb-0 px-4 py-4 flex justify-center items-center'>
             {successMessage !== false && (
               <div
                 className='bg-blue-100 border-t border-b border-blue-500 text-blue-700 px-4 py-3'
@@ -76,243 +76,177 @@ function RegisterClient() {
                 }}
                 onSubmit={(values) => handleSubmit(values)}
               >
-                <Form className='flex flex-wrap' method='POST'>
-                  <div className='w-full md:w-1/3 personal-details px-3'>
+                <Form className='w-full' method='POST'>
+                  <div className='w-full personal-details px-3'>
                     <h3 className='uppercase tracking-wide text-gray-700 text-md font-bold mb-3'>
                       Your details
                     </h3>
                     <div className='flex flex-wrap -mx-3 mb-6'>
-                      <div className='w-full md:w-1/2 mb-6 md:pr-3 md:mb-6'>
+                      <div className='w-full flex'>
+                        <div className='w-full  mb-6 md:pr-3 md:mb-6'>
+                          <label
+                            className='block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2'
+                            htmlFor='grid-first-name'
+                          >
+                            First Name
+                          </label>
+                          <Field
+                            name='first_name'
+                            className={`form-input w-full ${
+                              apiErrors.hasOwnProperty('first_name') &&
+                              typeof apiErrors.first_name[0] !== 'undefined'
+                                ? `border-red-500`
+                                : `border-gray-300`
+                            }`}
+                            id='grid-first-name'
+                            type='text'
+                            placeholder='Jane'
+                          />
+                          {apiErrors.hasOwnProperty('first_name') &&
+                            typeof apiErrors.first_name[0] !== 'undefined' && (
+                              <p className='text-red-500 text-12'>
+                                {apiErrors.first_name[0]}
+                              </p>
+                            )}
+                        </div>
+                        <div className='w-full md:pl-3'>
+                          <label
+                            className='block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2'
+                            htmlFor='grid-last-name'
+                          >
+                            Last Name
+                          </label>
+                          <Field
+                            name='last_name'
+                            className={`form-input w-full ${
+                              apiErrors.hasOwnProperty('last_name') &&
+                              typeof apiErrors.last_name[0] !== 'undefined'
+                                ? `border-red-500`
+                                : `border-gray-300`
+                            }`}
+                            id='grid-last-name'
+                            type='text'
+                            placeholder='Doe'
+                          />
+                          {apiErrors.hasOwnProperty('last_name') &&
+                            typeof apiErrors.last_name[0] !== 'undefined' && (
+                              <p className='text-red-500 text-12'>
+                                {apiErrors.last_name[0]}
+                              </p>
+                            )}
+                        </div>
+                      </div>
+                      <div className='w-full flex'>
+                        <div className='w-full  mb-6 md:pr-3 md:mb-6 email'>
+                          <label
+                            className='block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2'
+                            htmlFor='grid-last-name'
+                          >
+                            Email
+                          </label>
+                          <Field
+                            name='email'
+                            className={`form-input w-full ${
+                              apiErrors.hasOwnProperty('email') &&
+                              typeof apiErrors.email[0] !== 'undefined'
+                                ? `border-red-500`
+                                : `border-gray-300`
+                            }`}
+                            id='grid-last-name'
+                            type='email'
+                            placeholder='example@gmail.com'
+                          />
+                          {apiErrors.hasOwnProperty('email') &&
+                            typeof apiErrors.email[0] !== 'undefined' && (
+                              <p className='text-red-500 text-12'>
+                                {apiErrors.email[0]}
+                              </p>
+                            )}
+                        </div>
+                        <div className='w-full md:pl-3 phone'>
+                          <label
+                            className='block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2'
+                            htmlFor='grid-last-name'
+                          >
+                            Phone Number
+                          </label>
+                          <Field
+                            name='phone'
+                            className={`form-input w-full ${
+                              apiErrors.hasOwnProperty('phone') &&
+                              typeof apiErrors.phone[0] !== 'undefined'
+                                ? `border-red-500`
+                                : `border-gray-300`
+                            }`}
+                            id='grid-last-name'
+                            type='phone'
+                            placeholder='+40770009770'
+                          />
+                          {apiErrors.hasOwnProperty('phone') &&
+                            typeof apiErrors.phone[0] !== 'undefined' && (
+                              <p className='text-red-500 text-12'>
+                                {apiErrors.phone[0]}
+                              </p>
+                            )}
+                        </div>
+                      </div>
+                      <div className='w-full flex flex-wrap  mb-6 password'>
                         <label
                           className='block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2'
-                          htmlFor='grid-first-name'
+                          htmlFor='grid-password'
                         >
-                          First Name
+                          Password
                         </label>
                         <Field
-                          name='first_name'
+                          name='password'
                           className={`form-input w-full ${
-                            apiErrors.hasOwnProperty('first_name') &&
-                            typeof apiErrors.first_name[0] !== 'undefined'
+                            apiErrors.hasOwnProperty('password') &&
+                            typeof apiErrors.password[0] !== 'undefined'
                               ? `border-red-500`
                               : `border-gray-300`
                           }`}
-                          id='grid-first-name'
-                          type='text'
-                          placeholder='Jane'
+                          id='grid-password'
+                          type='password'
+                          placeholder='***'
                         />
-                        {apiErrors.hasOwnProperty('first_name') &&
-                          typeof apiErrors.first_name[0] !== 'undefined' && (
+                        {apiErrors.hasOwnProperty('password') &&
+                          typeof apiErrors.password[0] !== 'undefined' && (
                             <p className='text-red-500 text-12'>
-                              {apiErrors.first_name[0]}
+                              {apiErrors.password[0]}
                             </p>
                           )}
                       </div>
-                      <div className='w-full md:w-1/2 md:pl-3'>
+                      <div className='w-full flex flex-wrap mb-6 password'>
                         <label
                           className='block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2'
-                          htmlFor='grid-last-name'
+                          htmlFor='grid-password'
                         >
-                          Last Name
+                          Confirm Password
                         </label>
                         <Field
-                          name='last_name'
+                          name='password_confirmation'
                           className={`form-input w-full ${
-                            apiErrors.hasOwnProperty('last_name') &&
-                            typeof apiErrors.last_name[0] !== 'undefined'
+                            apiErrors.hasOwnProperty('password_confirmation') &&
+                            typeof apiErrors.password_confirmation[0] !==
+                              'undefined'
                               ? `border-red-500`
                               : `border-gray-300`
                           }`}
-                          id='grid-last-name'
-                          type='text'
-                          placeholder='Doe'
+                          id='grid-password'
+                          type='password'
+                          placeholder='***'
                         />
-                        {apiErrors.hasOwnProperty('last_name') &&
-                          typeof apiErrors.last_name[0] !== 'undefined' && (
-                            <p className='text-red-500 text-12'>
-                              {apiErrors.last_name[0]}
-                            </p>
-                          )}
-                      </div>
-                      <div className='w-full flex flex-wrap -mx-3 mb-6 px-3 email'>
-                        <label
-                          className='block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2'
-                          htmlFor='grid-last-name'
-                        >
-                          Email
-                        </label>
-                        <Field
-                          name='email'
-                          className={`form-input w-full ${
-                            apiErrors.hasOwnProperty('email') &&
-                            typeof apiErrors.email[0] !== 'undefined'
-                              ? `border-red-500`
-                              : `border-gray-300`
-                          }`}
-                          id='grid-last-name'
-                          type='email'
-                          placeholder='example@gmail.com'
-                        />
-                        {apiErrors.hasOwnProperty('email') &&
-                          typeof apiErrors.email[0] !== 'undefined' && (
-                            <p className='text-red-500 text-12'>
-                              {apiErrors.email[0]}
-                            </p>
-                          )}
-                      </div>
-                    </div>
-                  </div>
-                  <div className='w-full md:w-1/3 contact-details px-3'>
-                    <h3 className='uppercase tracking-wide text-gray-700 text-md font-bold mb-3'>
-                      Contact details
-                    </h3>
-                    <div className='flex flex-wrap -mx-3 mb-6 px-3 phone'>
-                      <label
-                        className='block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2'
-                        htmlFor='grid-last-name'
-                      >
-                        Phone Number
-                      </label>
-                      <Field
-                        name='phone'
-                        className={`form-input w-full ${
-                          apiErrors.hasOwnProperty('phone') &&
-                          typeof apiErrors.phone[0] !== 'undefined'
-                            ? `border-red-500`
-                            : `border-gray-300`
-                        }`}
-                        id='grid-last-name'
-                        type='phone'
-                        placeholder='+40770009770'
-                      />
-                      {apiErrors.hasOwnProperty('phone') &&
-                        typeof apiErrors.phone[0] !== 'undefined' && (
-                          <p className='text-red-500 text-12'>
-                            {apiErrors.phone[0]}
-                          </p>
-                        )}
-                    </div>
-                    <div className='flex flex-wrap -mx-3 mb-6 px-3 country'>
-                      <label
-                        className='block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2'
-                        htmlFor='grid-state'
-                      >
-                        Country
-                      </label>
-                      <div className='w-full'>
-                        <Field
-                          name='country'
-                          className={`form-input w-full ${
-                            apiErrors.hasOwnProperty('country') &&
-                            typeof apiErrors.country[0] !== 'undefined'
-                              ? `border-red-500`
-                              : `border-gray-300`
-                          }`}
-                          id='grid-state'
-                          as='select'
-                        >
-                          <option>MD</option>
-                          <option>RO</option>
-                          <option>US</option>
-                        </Field>
-                      </div>
-                      {apiErrors.hasOwnProperty('country') &&
-                        typeof apiErrors.country[0] !== 'undefined' && (
-                          <p className='text-red-500 text-12'>
-                            {apiErrors.country[0]}
-                          </p>
-                        )}
-                    </div>
-
-                    <div className='flex flex-wrap -mx-3 mb-6 location-details'>
-                      <div className='w-full px-3 mb-6 md:mb-0'>
-                        <label
-                          className='block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2'
-                          htmlFor='grid-city'
-                        >
-                          City
-                        </label>
-                        <Field
-                          name='city'
-                          className={`form-input w-full ${
-                            apiErrors.hasOwnProperty('city') &&
-                            typeof apiErrors.city[0] !== 'undefined'
-                              ? `border-red-500`
-                              : `border-gray-300`
-                          }`}
-                          id='grid-city'
-                          type='text'
-                          placeholder='Brasov'
-                        />
-                        {apiErrors.hasOwnProperty('city') &&
-                          typeof apiErrors.city[0] !== 'undefined' && (
-                            <p className='text-red-500 text-12'>
-                              {apiErrors.city[0]}
-                            </p>
-                          )}
-                      </div>
-                    </div>
-                  </div>
-                  <div className='w-full md:w-1/3 security'>
-                    <h3 className='uppercase tracking-wide text-gray-700 text-md font-bold mb-3'>
-                      Security
-                    </h3>
-
-                    <div className='flex flex-wrap -mx-3 mb-6 px-3 password'>
-                      <label
-                        className='block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2'
-                        htmlFor='grid-password'
-                      >
-                        Password
-                      </label>
-                      <Field
-                        name='password'
-                        className={`form-input w-full ${
-                          apiErrors.hasOwnProperty('password') &&
-                          typeof apiErrors.password[0] !== 'undefined'
-                            ? `border-red-500`
-                            : `border-gray-300`
-                        }`}
-                        id='grid-password'
-                        type='password'
-                        placeholder='***'
-                      />
-                      {apiErrors.hasOwnProperty('password') &&
-                        typeof apiErrors.password[0] !== 'undefined' && (
-                          <p className='text-red-500 text-12'>
-                            {apiErrors.password[0]}
-                          </p>
-                        )}
-                    </div>
-                    <div className='flex flex-wrap -mx-3 mb-6 px-3 password'>
-                      <label
-                        className='block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2'
-                        htmlFor='grid-password'
-                      >
-                        Confirm Password
-                      </label>
-                      <Field
-                        name='password_confirmation'
-                        className={`form-input w-full ${
-                          apiErrors.hasOwnProperty('password_confirmation') &&
+                        {apiErrors.hasOwnProperty('password_confirmation') &&
                           typeof apiErrors.password_confirmation[0] !==
-                            'undefined'
-                            ? `border-red-500`
-                            : `border-gray-300`
-                        }`}
-                        id='grid-password'
-                        type='password'
-                        placeholder='***'
-                      />
-                      {apiErrors.hasOwnProperty('password_confirmation') &&
-                        typeof apiErrors.password_confirmation[0] !==
-                          'undefined' && (
-                          <p className='text-red-500 text-12'>
-                            {apiErrors.password_confirmation[0]}
-                          </p>
-                        )}
+                            'undefined' && (
+                            <p className='text-red-500 text-12'>
+                              {apiErrors.password_confirmation[0]}
+                            </p>
+                          )}
+                      </div>
                     </div>
                   </div>
+
                   <div className='w-full flex justify-center'>
                     <button
                       disabled={isLoading ? true : undefined}
